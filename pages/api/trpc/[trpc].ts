c7a68e31f88637351ac52734dad8c117bd341996
Benny Hinrichs <hinrichs.benny@gmail.com>
2022-07-24 00:19:11 -0600

    make demo using Next.js, MUI, and tRPC

---
+++ pages/api/trpc/[trpc].ts
@@ -0,0 +1,36 @@
+import * as trpc from "@trpc/server";
+import * as trpcNext from "@trpc/server/adapters/next";
+import { z } from "zod";
+
+export const appRouter = trpc
+  .router()
+  .query("hello", {
+    input: z
+      .object({
+        text: z.string().nullish(),
+      })
+      .nullish(),
+    resolve({ input }) {
+      return {
+        greeting: `hello ${input?.text ?? "world"}`,
+      };
+    },
+  })
+  .mutation("multiply", {
+    input: z.object({
+      first: z.number(),
+      second: z.number(),
+    }),
+    resolve({ input }) {
+      return { product: input.first * input.second };
+    },
+  });
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
+
+// export API handler
+export default trpcNext.createNextApiHandler({
+  router: appRouter,
+  createContext: () => null,
+});
